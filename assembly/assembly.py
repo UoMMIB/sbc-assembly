2f60d947e3c8ad37e44a8315b79ec37974be2f96
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-12 12:57:01 +0100

    Added support for multiple recipes.

--- assembly/assembly.py
+++ assembly/assembly.py
@@ -9,13 +9,19 @@ import json
 import sys
 
 
-def assemble(recipe, level=0):
+def assemble(recipes):
     '''Assembles a recipe.'''
+    for recipe in recipes:
+        _assemble(recipe)
+
+
+def _assemble(recipe, level=0):
+    '''Recursively assembles a recipe.'''
     print str(level) + '\t' + str(recipe)
 
     for term in recipe[0]:
         if isinstance(term, tuple):
-            assemble(term, level + 1)
+            _assemble(term, level + 1)
 
 
 def main(args):
@@ -24,7 +30,7 @@ def main(args):
     b = (('A', 50), ('C', 17.5))
     c = (('A', 35), (b, 27.5))
     p = ((('X', 3), (a, 2), (c, 4)), 0)
-    assemble(p)
+    assemble([p, (c, 0)])
 
 
 if __name__ == '__main__':
