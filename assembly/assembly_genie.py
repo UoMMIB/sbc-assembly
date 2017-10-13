8e7662306885d90e46fc08a1f94b873524c242c8
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-13 09:42:33 +0100

    Simplified assembly code.

--- assembly/assembly_genie.py
+++ assembly/assembly_genie.py
@@ -29,32 +29,30 @@ class AssemblyGenie(object):
         '''Gets mappings.'''
         return self.__mappings
 
-    def __assemble(self, components, level=0, dest=None):
+    def __assemble(self, comp_vol, level=0, dest=None):
         '''Recursively assembles a recipe.'''
-        plate_idx, well, obj = self.__add(level, components)
-        new_dest = (plate_idx, well, obj)
-        # print str(new_dest) + '\t->\t' + str(dest)
+        plate_idx, well = self.__add(level, comp_vol[0])
+        new_dest = (plate_idx, well, comp_vol[0])
 
         if dest:
-            self.__mappings.append((components[0], dest[2], components[1]))
+            self.__mappings.append((comp_vol[0], dest[2], comp_vol[1]))
 
-        for component in components[0]:
+        for component in comp_vol[0]:
             if isinstance(component, tuple):
                 self.__assemble(component, level + 1, new_dest)
 
-    def __add(self, plate_idx, obj):
+    def __add(self, plate_idx, component):
         '''Add object to plate.'''
         if plate_idx >= len(self.__plates):
             self.__plates.append(Plate(plate_idx))
 
-        component = obj[0]
         found = self.__find(component)
 
         if found:
-            return tuple(found[0] + [component])
+            return tuple(found[0])
 
         well = self.__plates[plate_idx].add(component)
-        return plate_idx, well, component
+        return plate_idx, well
 
     def __find(self, obj):
         '''Finds an object in Plates.'''
@@ -67,18 +65,27 @@ class AssemblyGenie(object):
         return locations
 
 
-def main():
-    '''main method.'''
+def _get_test_recipes():
+    '''Get test recipes.'''
     a = (('A', 5), ('B', 7.5))
     b = (('A', 50), ('C', 17.5))
     c = (('A', 35), (b, 27.5))
     p = ((('X', 3), (a, 2), (c, 4)), 0)
 
-    ass_gen = AssemblyGenie([p, (c, 0)])
+    return [p, (c, 0)]
+
+
+def main():
+    '''main method.'''
+    recipes = _get_test_recipes()
+
+    ass_gen = AssemblyGenie(recipes)
     ass_gen.assemble()
 
     for plate in ass_gen.get_plates():
         print plate
+
+    print
 
     for mapping in ass_gen.get_mappings():
         print mapping
