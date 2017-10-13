e2cb41465b3de80beb8a61ee2cd32512b35d65f2
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-13 14:13:35 +0100

    Added tree plotter.

---
+++ assembly/tree_plotter.py
@@ -0,0 +1,50 @@
+'''
+AssemblyGenie (c) GeneGenie 2017
+
+All rights reserved.
+
+@author: neilswainston
+'''
+import sys
+
+from igraph import Graph
+import matplotlib.pyplot as plt
+
+
+def plot(labels, tree):
+    '''Plot labelled tree.'''
+    lay = tree.layout('tree', 0)
+    position = {k: lay[k] for k in range(len(lay.coords))}
+
+    _, ax = plt.subplots()
+    ax.axis('off')
+
+    for edge in [e.tuple for e in tree.es]:
+        X = [position[edge[0]][0], position[edge[1]][0]]
+        Y = [position[edge[0]][1], position[edge[1]][1]]
+        plt.plot(X, Y, lw=1, color='gray')
+
+    xs, ys = zip(*position.values())
+    ax.plot(xs, ys, 'o')
+
+    for x, y, label in zip(xs, ys, labels):
+        ax.annotate(label, xy=(x, y))
+
+    plt.show()
+
+
+def _get_tree(vertices=16, children=2):
+    '''Gets tree.'''
+    labels = map(str, range(vertices))
+    tree = Graph.Tree(vertices, children)
+    return labels, tree
+
+
+def main(args):
+    '''main method.'''
+    labels, tree = _get_tree(int(args[0]), int(args[1]))
+    plot(labels, tree)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
