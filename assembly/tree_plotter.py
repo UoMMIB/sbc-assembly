16067d9d04cc8ca652222e925905f213cf1d1c39
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-17 12:27:55 +0100

    Cosmetic improvements.

--- assembly/tree_plotter.py
+++ assembly/tree_plotter.py
@@ -17,28 +17,28 @@ def plot(labels, tree, markersize=24):
     lay = tree.layout('tree', 0)
     position = {k: lay[k] for k in range(len(lay.coords))}
 
-    _, ax = plt.subplots()
-    ax.axis('off')
-
-    xs, ys = zip(*position.values())
-    plt.plot(xs, ys,
-             'o',
-             markersize=markersize,
-             markerfacecolor='lightblue',
-             markeredgecolor='grey')
+    # _, ax = plt.subplots()
+    # ax.axis('off')
 
     for edge in [e.tuple for e in tree.es]:
         x = [position[edge[0]][0], position[edge[1]][0]]
         y = [position[edge[0]][1], position[edge[1]][1]]
-        # plt.plot(x, y, lw=1, color='gray')
         plt.arrow(x[0], y[0], x[1] - x[0], y[1] - y[0],
-                  head_width=0.05,
+                  head_width=0.1,
                   head_length=0.1,
                   fc='grey',
                   ec='grey')
 
+    xs, ys = zip(*position.values())
+    plt.plot(xs, ys,
+             'o',
+             alpha=0.25,
+             markersize=markersize,
+             markerfacecolor='navy',
+             markeredgecolor='black')
+
     for x, y, label in zip(xs, ys, labels):
-        ax.annotate(label, xy=(x - 0.075, y - 0.075))
+        plt.annotate(label, xy=(x, y))
 
     plt.show()
 
