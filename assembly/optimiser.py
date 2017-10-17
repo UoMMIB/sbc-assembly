e324bb589b34f560e1d59a23d0df71f094d7d836
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-17 10:39:39 +0100

    Integrated optimise step.

--- assembly/optimiser.py
+++ assembly/optimiser.py
@@ -18,24 +18,46 @@ class Optimiser(object):
         self.__drop()
         print self.__df
 
+    def optimise(self):
+        '''Optimise dataframe.'''
+        while True:
+            mask = None
+            max_match_col = pd.Series(0.0, index=self.__df.index)
+
+            for idx, col1 in enumerate(self.__df.columns):
+                for col2 in self.__df[self.__df.columns[idx + 1:]]:
+                    match_col = self.__df[col1] * \
+                        (self.__df[col1] == self.__df[col2])
+
+                    if sum(match_col.astype(bool)) > \
+                            sum(max_match_col.astype(bool)):
+                        mask = pd.DataFrame(0.0, columns=self.__df.columns,
+                                            index=self.__df.index)
+                        mask[col1] = match_col.values
+                        max_match_col = match_col
+
+                    if match_col.sum() and (match_col == max_match_col).all():
+                        mask[col2] = match_col.values
+
+            if mask is None or max(mask.astype(bool).sum()) < 2:
+                break
+
+            self.__add_intermediate(mask, max_match_col)
+
+        print self.__df
+
     def __get_components(self, comps, vol, dest=None):
         '''Gets components.'''
-        print str(comps) + '\t' + str(vol) + '\t' + str(dest)
-
         if isinstance(comps, str):
             comp_id = comps
             self.__add_row_col(comp_id)
         else:
-            comp_id = '_i' + str(self.__intermediates)
-            self.__intermediates += 1
+            comp_id = self.__get_intermediate_name()
 
             self.__add_row_col(comp_id)
 
             for comp in comps:
-                try:
-                    self.__get_components(comp[0], comp[1], comp_id)
-                except IndexError, e:
-                    print e
+                self.__get_components(comp[0], comp[1], comp_id)
 
         if dest:
             self.__df[dest][comp_id] = vol
@@ -49,7 +71,7 @@ class Optimiser(object):
         '''Add new row and column.'''
         if self.__df.empty:
             self.__df[comp_id] = pd.Series([0.0], index=[comp_id])
-        elif comp_id not in self._Optimiser__df:
+        elif comp_id not in self.__df:
             # Add row:
             new_row = pd.Series([0.0] * len(self.__df.columns),
                                 index=self.__df.columns)
@@ -60,59 +82,34 @@ class Optimiser(object):
             self.__df[comp_id] = pd.Series([0.0] * len(self.__df.index),
                                            index=self.__df.index)
 
+    def __add_intermediate(self, mask, max_match_col):
+        '''Adds an intermediate.'''
+        self.__df = self.__df - mask
+        new_id = self.__get_intermediate_name()
+        self.__df[new_id] = max_match_col
+        new_row = mask.any().astype(float)
+        new_row.name = new_id
+        self.__df = self.__df.append(new_row)
+        self.__df = self.__df.fillna(0)
+        self.__drop()
 
-def optimise(ingredients):
-    '''Optimise dataframe.'''
-    df = _init(ingredients)
-
-    while True:
-        mask = None
-        max_match_col = pd.Series(0.0, index=df.index)
-
-        for idx, col1 in enumerate(df.columns):
-            for col2 in df[df.columns[idx + 1:]]:
-                match_col = df[col1] * (df[col1] == df[col2])
-
-                if sum(match_col.astype(bool)) > \
-                        sum(max_match_col.astype(bool)):
-                    mask = pd.DataFrame(0.0, columns=df.columns,
-                                        index=df.index)
-                    mask[col1] = match_col.values
-                    max_match_col = match_col
-
-                if match_col.sum() and (match_col == max_match_col).all():
-                    mask[col2] = match_col.values
-
-        if mask is None or max(mask.astype(bool).sum()) < 2:
-            break
-
-        df = _add_intermediate(df, mask, max_match_col)
-
-    return df
-
-
-def _add_intermediate(df, mask, max_match_col):
-    '''Adds an intermediate.'''
-    df = df - mask
-    new_id = str(uuid.uuid4())
-    df[new_id] = max_match_col
-    new_row = mask.any().astype(float)
-    new_row.name = new_id
-    df = df.append(new_row)
-    df = df.fillna(0)
-    return _drop(df)
+    def __get_intermediate_name(self):
+        '''Get unique intermediate name.'''
+        int_id = '_i' + str(self.__intermediates)
+        self.__intermediates += 1
+        return int_id
 
 
 def main():
     '''main method.'''
     ingredients = (
         (((((('K', 1), ('L', 2)), 3), ('D', 4), ('E', 5), ('F', 6)), 0),
-         ((('D', 7), ('E', 8), ('F', 9), ('Y', 10)), 0),
-         ((('D', 11), ('E', 12), ('F', 13), ('Z', 14)), 0)
+         ((('D', 4), ('E', 5), ('F', 6), ('Y', 10)), 0),
+         ((('D', 4), ('E', 5), ('F', 13), ('Z', 14)), 0)
          ), 0)
 
     optim = Optimiser(ingredients)
-    # df = optimise(ingredients)
+    optim.optimise()
 
 
 if __name__ == '__main__':
