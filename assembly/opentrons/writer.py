d614bc5bc693829ae1923e0f5964ce763fc87219
neilswainston <neil.swainston@manchester.ac.uk>
2018-11-07 10:04:42 +0000

    Minor updates to Opentrons writer.

--- assembly/opentrons/writer.py
+++ assembly/opentrons/writer.py
@@ -15,49 +15,54 @@ from assembly import plate
 import pandas as pd
 
 
-def write(filename):
-    '''Write commands from worklist csv.'''
-    worklist_df = pd.read_csv(filename)
+class OpentronsWriter():
+    '''Class representing an Opentrons writer.'''
 
-    deck = plate.Plate('deck', rows=5, cols=3)
+    def __init__(self, rows=4, cols=3):
+        self.__deck = plate.Plate('deck', rows=rows, cols=cols)
+        self.__tip_racks = None
 
-    tip_racks = _add_tip_racks(deck, len(worklist_df))
+    def write(self, filename):
+        '''Write commands from worklist csv.'''
+        wklst_df = pd.read_csv(filename)
 
-    plt_names = ['SourcePlateBarcode', 'DestinationPlateBarcode']
-    plates = {name: _add_plate(deck, '96-flat', name)
-              for name in pd.unique(worklist_df[plt_names].values.ravel('K'))}
+        # Add tip-racks:
+        self.__add_tip_racks(len(wklst_df))
 
-    # pipettes
-    pipette = instruments.Pipette(
-        axis='b', max_volume=200, tip_racks=tip_racks)
+        plt_names = ['SourcePlateBarcode', 'DestinationPlateBarcode']
+        plates = {name: self.__add_plate('96-flat', name)
+                  for name in pd.unique(wklst_df[plt_names].values.ravel())}
 
-    # commands
-    for _, row in worklist_df.iterrows():
-        pipette.transfer(row['Volume'],
-                         plates[row['SourcePlateBarcode']].wells(
-                             row['SourcePlateWell']),
-                         plates[row['DestinationPlateBarcode']].wells(
-                             row['DestinationPlateWell']))
+        #
+        pipette = instruments.Pipette(tip_racks=self.__tip_racks)
 
+        # commands
+        for _, row in wklst_df.iterrows():
+            pipette.transfer(row['Volume'],
+                             plates[row['SourcePlateBarcode']].wells(
+                                 row['SourcePlateWell']),
+                             plates[row['DestinationPlateBarcode']].wells(
+                                 row['DestinationPlateWell']))
 
-def _add_tip_racks(deck, len_worklist, typ='tiprack-200ul'):
-    '''Add tipracks to deck.'''
-    plt = _add_plate(deck, typ, 'tiprack_0')
-    plt_size = len(plt.rows) * len(plt.cols)
+    def __add_tip_racks(self, len_worklist, typ='tiprack-200ul'):
+        '''Add tipracks to deck.'''
+        plt = self.__add_plate(typ, 'tiprack_0')
+        plt_size = len(plt.rows) * len(plt.cols)
 
-    return [plt] + [_add_plate(deck, typ, 'tiprack_' + str(idx))
-                    for idx in range(1, math.ceil(len_worklist / plt_size))]
+        self.__tip_racks = [plt] + \
+            [self.__add_plate(typ, 'tiprack_' + str(idx))
+             for idx in range(1, math.ceil(len_worklist / plt_size))]
 
-
-def _add_plate(deck, typ, name):
-    '''Add plate to deck.'''
-    pos = deck.add(name)
-    return containers.load(typ, pos, name)
+    def __add_plate(self, typ, name):
+        '''Add plate to deck.'''
+        pos = self.__deck.add(name)
+        return containers.load(typ, pos, name)
 
 
 def main(args):
     '''main method.'''
-    write(args[0])
+    writer = OpentronsWriter()
+    writer.write(args[0])
 
     for command in robot.commands():
         print(command)
