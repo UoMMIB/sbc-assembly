1553436260d63d6978e3b3ddb9d163507b594eb3
neilswainston <neil.swainston@manchester.ac.uk>
2018-11-07 13:41:30 +0000

    Updated primers writer.

--- assembly/app/lcr/primers.py
+++ assembly/app/lcr/primers.py
@@ -12,7 +12,6 @@ All rights reserved.
 from collections import defaultdict
 import sys
 
-from Bio.Seq import Seq
 from synbiochem.utils import dna_utils, seq_utils
 
 from assembly import plate
@@ -28,68 +27,114 @@ class PrimerDesigner():
                                             ice_details['username'],
                                             ice_details['password'])
 
-    def get_primers(self, plasmid_ids, plates, restr_enz, tm, mg_conc):
+    def get_primers(self, plasmid_ids, plates, restr_enz, tm):
         '''Get primers for Parts by id.'''
-        plasmid_parts = self.__ice_helper.get_plasmid_parts(plasmid_ids,
-                                                            type_filter='PART')
+        plasmid_parts = \
+            self.__ice_helper.get_plasmid_parts(plasmid_ids,
+                                                type_filter='PART')
 
-        plasmid_primers = _get_primers(plasmid_parts, restr_enz, tm, mg_conc)
+        parts = {}
 
-        return _get_plates(plasmid_primers, plates)
+        for dct in plasmid_parts.values():
+            parts.update(dct)
+
+        primers = _get_primers(parts, restr_enz, tm)
+
+        return self.__get_plates(primers, plates)
+
+    def __get_plates(self, primers, plates):
+        '''Map primers to plates.'''
+        primer_plates = defaultdict(list)
+
+        for _, values in primers.items():
+            primer_id, plate_id, row_col = \
+                self.__get_location(values[0], plates,
+                                    [plate.Plate('primer_1')])
+
+            row, col = plate.get_indices(row_col)
+
+            nonphospho = primer_plates[plate_id + '_primer_nonphospho']
+            phospho = primer_plates[plate_id + '_primer_phospho']
+            nonphospho.append(
+                [row_col, primer_id + '_F', values[1][0], row, col])
+            nonphospho.append(
+                [row_col, primer_id + '_R', values[2][0], row, col])
+            phospho.append(
+                [row_col, primer_id + '_F', '/5Phos/' + values[1][0],
+                 row, col])
+            phospho.append(
+                [row_col, primer_id + '_R', '/5Phos/' + values[2][0],
+                 row, col])
+
+            print(phospho)
+
+        columns = ['Well Position', 'Sequence Name',
+                   'Sequence', 'row', 'column']
+
+        for plate_id in primer_plates:
+            df = pd.DataFrame(primer_plates[plate_id], columns=columns)
+            df.sort_values(['row', 'column'], inplace=True)
+            df.drop(['row', 'column'], axis=1, inplace=True)
+            primer_plates[plate_id] = df
+
+        return primer_plates
+
+    def __get_location(self, part, existing_plates, new_plates):
+        '''Get location.'''
+        parent_plasmid = self.__get_parent_plasmid(part)
+
+        if parent_plasmid is None:
+            # Special case: legacy material do not have parent plasmids
+            # in ICE (or perhaps even in reality).
+            parent_plasmid_id = part.get_ice_id()
+        else:
+            parent_plasmid_id = parent_plasmid.get_ice_id()
+
+        part_locs = plate.find(existing_plates, parent_plasmid_id)
+
+        if part_locs:
+            plate_id = part_locs.keys()[0]
+            row_col = part_locs.values()[0][0]
+        else:
+            # No location found: return default plate entry:
+            plt = new_plates[-1]
+            plate_id = plt.get_name()
+            row_col = plt.add({'id': parent_plasmid_id})
+
+        return parent_plasmid_id, plate_id, row_col
+
+    def __get_parent_plasmid(self, part_ice):
+        '''Get "parent" Plasmid from Part.'''
+        part_metadata = part_ice.get_metadata()
+
+        for parent in part_metadata['parents']:
+            if parent['visible'] == 'OK':
+                parent = self.__ice_helper.get_ice_entry(parent['id'])
+                linked_part_ids = \
+                    [linked_part['id']
+                     for linked_part in parent.get_metadata()['linkedParts']]
+
+                if len(linked_part_ids) == 2 and \
+                        part_metadata['id'] in linked_part_ids:
+                    return parent
+
+        return None
 
 
-def _get_primers(plasmid_parts, restr_enz, tm, mg_conc):
+def _get_primers(parts, restr_enz, tm):
     '''Design primers.'''
-    plasmid_primers = defaultdict(list)
-    reag_conc = {seq_utils.MG: mg_conc}
+    primers = {}
 
-    for plasmid_id in plasmid_parts:
-        parts = plasmid_parts[plasmid_id]
-        digest = _apply_restricts(parts.values()[0].get_dna(),
-                                  restr_enz)['seq']
-        rev_comp = str(Seq(digest).reverse_complement())
-        plasmid_primers[plasmid_id] = \
-            [seq_utils.get_seq_by_melt_temp(digest, tm,
-                                            reagent_concs=reag_conc),
-             seq_utils.get_seq_by_melt_temp(rev_comp, tm,
-                                            reagent_concs=reag_conc)]
+    for part_id, part in parts.items():
+        digest = _apply_restricts(part.get_dna(), restr_enz)['seq']
 
-    return plasmid_primers
+        primers[part_id] = [
+            part,
+            seq_utils.get_seq_by_melt_temp(digest, tm),
+            seq_utils.get_seq_by_melt_temp(digest, tm, forward=False)
+        ]
 
-
-def _get_plates(plasmid_primers, plates):
-    '''Map primers to plates.'''
-    primer_plates = defaultdict(list)
-
-    for plasmid_id, primer_pair in plasmid_primers.items():
-        part_locs = plate.find(plates, plasmid_id)
-        plate_id = part_locs.keys()[0]
-        row_col = part_locs.values()[0][0]
-        row, col = plate.get_indices(row_col)
-
-        nonphospho = primer_plates[plate_id + '_primer_nonphospho']
-        phospho = primer_plates[plate_id + '_primer_phospho']
-        nonphospho.append(
-            [row_col, plasmid_id + '_F', primer_pair[0][0], row, col])
-        nonphospho.append(
-            [row_col, plasmid_id + '_R', primer_pair[1][0], row, col])
-        phospho.append(
-            [row_col, plasmid_id + '_F', '/5Phos/' + primer_pair[0][0],
-             row, col])
-        phospho.append(
-            [row_col, plasmid_id + '_R', '/5Phos/' + primer_pair[1][0],
-             row, col])
-
-    columns = ['Well Position', 'Sequence Name',
-               'Sequence', 'row', 'column']
-
-    for plate_id in primer_plates:
-        df = pd.DataFrame(primer_plates[plate_id], columns=columns)
-        df.sort_values(['row', 'column'], inplace=True)
-        df.drop(['row', 'column'], axis=1, inplace=True)
-        primer_plates[plate_id] = df
-
-    return primer_plates
+    return primers
 
 
 def _apply_restricts(dna, restr_enz):
@@ -112,11 +157,12 @@ def main(args):
     designer = PrimerDesigner(ice_details)
 
     plates = [plate.from_table(pd.read_csv(filename), filename)
-              for filename in args[6].split(',')]
+              for filename in args[5].split(',')] \
+        if args[5] != 'None' else {}
 
-    primer_plates = designer.get_primers(args[7:], plates,
+    primer_plates = designer.get_primers(args[6:], plates,
                                          args[3].split(','),
-                                         float(args[4]), float(args[5]))
+                                         float(args[4]))
 
     for plate_id, plt in primer_plates.items():
         plt.to_csv(plate_id + '.csv', index=False, encoding='utf-8')
